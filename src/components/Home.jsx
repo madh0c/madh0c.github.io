69adbb53b4fa845497229ae5261d83533ba1418b
Jeffery <jdpan002@gmail.com>
2023-02-10 11:31:13 +1100

    started hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import HeroImage from '../assets/heroImage.png'
+import { AiFillCaretRight } from 'react-icons/ai'
 
 const Home = () => {
   return (
-    <div>
-        
+    <div name='home' className='h-screen w-full bg-gradient-to-b from-gray-900 to-cyan-800'>        
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+          <div className='flex flex-col justify-center h-full'>
+            <h2 className='text-4xl sm:text-7xl font-semibold text-white'>
+              Jeffery Pan
+            </h2>
+            <p className='text-gray-400 py-4 max-w-md'>
+              Comp-Sci Student & Aspiring Software Engineer
+            </p>
+            <div>
+              <button>
+                Portfolio
+                <span>
+                  <AiFillCaretRight />
+                </span>                
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <img src={HeroImage} alt="heroImg" className='rounded-full mx-auto w-2/3' />
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,9 +22,9 @@ const NavBar = () => {
   ];
 
   return (
-    <div className='flex justify-between items-center w-full h-24 px-4 text-white bg-black fixed'>
+    <div className='flex justify-between items-center w-full h-24 px-4 text-white fixed'>
       <div>
-        <img src={logo} alt='home button pic' className='w-1/3 md:flex cursor-pointer pr-20 hover:saturate-200'/>
+        <img src={logo} alt='home button pic' className='w-1/3 md:w-1/4 cursor-pointer pr-20 hover:saturate-200'/>
       </div>
 
       <ul className='hidden md:flex pr-4'>
@@ -32,13 +32,14 @@ const NavBar = () => {
             <li key={id} className='px-16 cursor-pointer text-2xl font-bold hover:scale-110 hover:text-cyan-300 duration-100'>{link}</li>
         ))}
       </ul>
-
+      
+      {/* mobile view */}
       <div className='flex md:hidden cursor-pointer z-10 text-gray-500' onClick={() => setNav(!nav)}>
         {nav ? <FaTimes size={30} /> : <FaBars size={30} /> }
-      </div>
-
+      </div>     
+      
       {nav && (
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg bg-gradient-to-b from-black to-cyan-800'>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg bg-gradient-to-b from-gray-900 to-cyan-800'>
         {links.map(({id, link}) => (
             <li key={id} className='px-4 cursor-pointer py-10 font-medium text-4xl'>{link}</li>
         ))}
