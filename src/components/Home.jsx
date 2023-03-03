2c0ca550c5e9e9953cf84ad458bfef6e38892931
Jeffery Pan <jdpan002@gmail.com>
2023-03-03 15:48:27 +1100

    projects skeleton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name='home' className='h-screen w-full bg-gradient-to-b from-gray-900 to-cyan-800'>        
         <div className='max-w-screen-lg px-4 mx-auto flex flex-col items-center justify-center h-full md:flex-row'>
           <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-4xl sm:text-6xl font-bold text-white'>
+            <h2 className='text-3xl sm:text-5xl font-bold text-white'>
               Hi, I'm Jeffery
             </h2>
             <p className='text-gray-400 py-4 max-w-md'>
@@ -24,7 +24,7 @@ const Home = () => {
           </div>
 
           <div>
-            <img src={HeroImage} alt="heroImg" className='rounded-full mx-auto w-1/2' />
+            <img src={HeroImage} alt="heroImg" className='rounded-full mx-auto place-self-auto w-[45%]' />
           </div>
         </div>
     </div>
