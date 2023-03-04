de8d4f522d860252dbae39e224c1d7efaffe395b
Jeffery <jdpan002@gmail.com>
2023-03-04 14:42:38 +1100

    linked buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import {FaBars, FaTimes} from 'react-icons/fa'
+import {Link} from 'react-scroll'
 
 const NavBar = () => {
 
@@ -9,31 +10,33 @@ const NavBar = () => {
   const links = [
     {
       id: 1,
-      link: 'About'
+      link: 'home'
     },
     {
       id: 2,
-      link: 'Projects'
+      link: 'about'
     },
     {
       id: 3,
-      link: 'Resume'
+      link: 'projects'
     },
     {
       id: 4,
-      link: 'Contact'
+      link: 'contact'
     }
   ];
 
   return (
     <div className='flex justify-between items-center w-full h-24 px-4 text-white fixed'>
-      <div>
+      <a href={'home'}>
         <img src={logo} alt='home button pic' className='w-1/3 md:w-1/4 cursor-pointer pr-20 hover:saturate-200'/>
-      </div>
+      </a>
 
       <ul className='hidden md:flex pr-4'>
         {links.map(({id, link}) => (
-            <li key={id} className='px-8 cursor-pointer text-l font-extrabold hover:scale-110 hover:text-cyan-300 duration-150'>{link}</li>
+            <li key={id} className='px-8 cursor-pointer text-l capitalize font-extrabold hover:scale-110 hover:text-cyan-300 duration-150'>
+              <Link to={link} smooth duration={500}>{link}</Link>
+            </li>
         ))}
       </ul>
       
@@ -45,7 +48,9 @@ const NavBar = () => {
       {nav && (
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-gray-900 to-cyan-800'>
         {links.map(({id, link}) => (
-            <li key={id} className='px-4 cursor-pointer py-10 font-medium text-4xl'>{link}</li>
+            <li key={id} className='px-4 cursor-pointer py-10 font-medium capitalize text-4xl'>
+              <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>  
+            </li>
         ))}
         </ul>
       )}
