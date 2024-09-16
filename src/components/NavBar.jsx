c3c9afec40e1c07a17182dcdb604108451e83a71
Jeffery Pan <jdpan002@gmail.com>
2024-09-16 13:05:05 +1000

    updated mobile view navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -62,12 +62,12 @@ const NavBar = () => {
       </nav>
 
       {/* mobile view */}
-      <div className='flex md:hidden cursor-pointer z-10 text-gray-500' onClick={() => setNav(!nav)}>
+      <div className='flex md:hidden cursor-pointer z-10  text-gray-500' onClick={() => setNav(!nav)}>
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-gray-900 to-cyan-800'>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-white dark:bg-slate-900'>
           {links.map(({ id, link }) => (
             <li key={id} className='px-4 cursor-pointer py-10 font-medium text-xl'>
               <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
