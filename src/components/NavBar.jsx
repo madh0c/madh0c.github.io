61ef7f6bd8735b63ff2614af8f96f19dbdd03e85
Jeffery Pan <jdpan002@gmail.com>
2024-09-16 13:08:35 +1000

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -47,7 +47,7 @@ const NavBar = () => {
           </div>
         ))}
 
-        <button onClick={toggleTheme} className="flex justify-center items-center dark:bg-purple-600 dark:hover:bg-purple-700 bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out mx-6 px-2 py-2 rounded-lg font-semibold">
+        <button onClick={toggleTheme} className="flex justify-center items-center dark:bg-purple-600 dark:hover:bg-purple-700 bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out mx-6 px-2 py-2 rounded-lg">
           <svg class="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
             <path class="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
             <path class="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
@@ -56,7 +56,6 @@ const NavBar = () => {
             <path class="fill-slate-400" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z" />
             <path class="fill-slate-500" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z" />
           </svg>
-          <span class="sr-only">Switch to light / dark version</span>
         </button>
 
       </nav>
@@ -73,6 +72,17 @@ const NavBar = () => {
               <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
             </li>
           ))}
+
+          <button onClick={toggleTheme} className="flex justify-center items-center dark:bg-purple-600 dark:hover:bg-purple-700 bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out px-10 py-5 rounded-lg">
+            <svg class="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+              <path class="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
+              <path class="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
+            </svg>
+            <svg class="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+              <path class="fill-slate-400" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z" />
+              <path class="fill-slate-500" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z" />
+            </svg>
+          </button>
         </ul>
       )}
 
